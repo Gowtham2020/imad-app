860ae0909819479fc124cb6d81b2a1a685f8f4c2
Gowtham2020 <30812292+Gowtham2020@users.noreply.github.com>
2017-09-07 19:01:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/article-one', function (req, res) {
-  res.send('application is to be created');
+  res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
 });
 app.get('/article-two', function (req, res) {
-  res.send('application is to be created');
+  res.sendFile(path.join(__dirname, 'ui', 'articletwo.html'));
 });
 app.get('/article-three', function (req, res) {
-  res.send('application is to be created');
+  res.sendFile(path.join(__dirname, 'ui', 'articlethree.html'));
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
