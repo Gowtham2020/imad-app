8c73df838ee14fee901645ab70b9f4d3f99badf2
Gowtham2020 <30812292+Gowtham2020@users.noreply.github.com>
2017-09-07 20:18:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,58 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles={
+    
+    'article-one':{title:'article-one',
+        heading:'Article-one',
+        content:`<p>
+    article-two:{title:'article-two',
+        heading:'Article-two',
+        content:`<p>lides: https://docs.google.com/presentation/d/19gCzh13_rZ8bpT_FTx27Gglu9jujjbTF0I8xrhMqpN0/ Forum: https://discourse.imad.hasura.io</p>
+<p>lides: https://docs.google.com/presentation/d/19gCzh13_rZ8bpT_FTx27Gglu9jujjbTF0I8xrhMqpN0/ Forum: https://discourse.imad.hasura.io</p>
+<p>lides: https://docs.google.com/presentation/d/19gCzh13_rZ8bpT_FTx27Gglu9jujjbTF0I8xrhMqpN0/ Forum: https://discourse.imad.hasura.io</p>
+`},</p>`
+},
+    'article-two':{title:'article-two',
+        heading:'Article-two',
+        content:`<p>lides: https://docs.google.com/presentation/d/19gCzh13_rZ8bpT_FTx27Gglu9jujjbTF0I8xrhMqpN0/ Forum: https://discourse.imad.hasura.io</p>
+<p>lides: https://docs.google.com/presentation/d/19gCzh13_rZ8bpT_FTx27Gglu9jujjbTF0I8xrhMqpN0/ Forum: https://discourse.imad.hasura.io</p>
+<p>lides: https://docs.google.com/presentation/d/19gCzh13_rZ8bpT_FTx27Gglu9jujjbTF0I8xrhMqpN0/ Forum: https://discourse.imad.hasura.io</p>
+`},
+     'article-three':{title:'article-three',
+        heading:'Article-three',
+        content:`<p>lides: https://docs.google.com/presentation/d/19gCzh13_rZ8bpT_FTx27Gglu9jujjbTF0I8xrhMqpN0/ Forum: https://discourse.imad.hasura.io</p>
+<p>lides: https://docs.google.com/presentation/d/19gCzh13_rZ8bpT_FTx27Gglu9jujjbTF0I8xrhMqpN0/ Forum: https://discourse.imad.hasura.io</p>
+<p>lides: https://docs.google.com/presentation/d/19gCzh13_rZ8bpT_FTx27Gglu9jujjbTF0I8xrhMqpN0/ Forum: https://discourse.imad.hasura.io</p>
+`}
+};
+function createtemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var content=data.coontent;
+    
+    var htmltemplate=`<html>
+<head>
+<title>$(title)</title>
+</head>
+
+<body>
+<div class='container'>
+<div>
+<h1>
+$(heading)
+</h1>
+</div>
+<div>
+$(content)
+</div>
+</div>
+</body>
+</html>
+
+    
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
