f431c9bbb901414cd6972ab7b9668aaaa6df5c9d
Gowtham2020 <30812292+Gowtham2020@users.noreply.github.com>
2017-09-08 13:52:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,6 +67,9 @@ app.get('/counter',function(req,res){
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/:articlename', function (req, res) {
     var articlename=req.params.articlename;
   res.send(createtemplate(articles[articlename]));
